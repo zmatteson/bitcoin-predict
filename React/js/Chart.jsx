40cd1eb67327208e7bc0ac4aad82577b50f455f0
Laura Godinez <lauragodinez15@gmail.com>
2018-04-19 17:33:22 -0500

    Updated some of style.css and rearrange some chart stuff

--- React/js/Chart.jsx
+++ React/js/Chart.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { HorizontalBar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
+
+const info = [65, 59, 80, 81, 56, 55, 45, 80, 100];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September'];
 
 const data = {
-	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September'],
+	labels: months,
 	datasets: [
 		{
 			label: 'My First dataset',
 			backgroundColor: 'rgba(255,99,132,0.2)',
 			borderColor: 'rgba(255,99,132,1)',
-			borderWidth: 1,
+			borderWidth: 2,
 			hoverBackgroundColor: 'rgba(255,99,132,0.4)',
 			hoverBorderColor: 'rgba(255,99,132,1)',
-			data: [65, 59, 80, 81, 56, 55, 45, 80, 100]
+			data: info
 		}
 	]
 };
@@ -22,8 +25,8 @@ const Chart = () => ({
 	render() {
 		return (
 			<div>
-				<h2>Horizontal Bar Example</h2>
-				<HorizontalBar data={data} />
+				<h2>Line Graph Example</h2>
+				<Line data={data} />
 			</div>
 		);
 	}
--- React/js/Landing.jsx
+++ React/js/Landing.jsx
@@ -11,15 +11,15 @@ const Landing = () => (
 		<h1> -</h1>
 		<h1> -</h1>
 		<h1> -</h1>
+		<Chart />
+		<Chart />
+		<Chart />
 		<h1>ABOUT</h1>
 		<p>
 			Bitcoin Predict is a new, web application that displays current bitcoin prices and utilize
 			deep learning neural networks to predict future bitcoin prices, which will then be displayed to the user
 			on a line graph.
 		</p>
-		<Chart />
-		<Chart />
-		<Chart />
 	</div>
 );
 
