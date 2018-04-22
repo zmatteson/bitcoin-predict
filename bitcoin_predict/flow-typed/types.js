43eb333dd4d575f00fbcc93c7b45befd0b804eef
Kevin Spencer <36053680+k-spencer@users.noreply.github.com>
2018-04-22 01:35:43 -0500

    django with react
    No actual changes here

---
+++ bitcoin_predict/flow-typed/types.js
@@ -0,0 +1,18 @@
+// @flow
+
+export type Show = {
+	title: string,
+	description: string,
+	year: string,
+	imdbID: string,
+	trailer: string,
+	poster: string
+};
+
+declare var module: {
+	hot: {
+		accept(path: string, callback: () => void): void
+	}
+};
+
+
