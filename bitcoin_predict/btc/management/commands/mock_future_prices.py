10bb512c69986f52c21baa41da71b12b48f5dc43
Zeb Matteson <zmatteson@gmail.com>
2018-04-26 00:47:29 -0500

    improved mocking data

--- bitcoin_predict/btc/management/commands/mock_future_prices.py
+++ bitcoin_predict/btc/management/commands/mock_future_prices.py
@@ -7,16 +7,19 @@ import requests
 from django.core.management.base import BaseCommand, CommandError
 import random
 
+def daterange(date1, date2):
+    for n in range(int ((date2 - date1).days)+1):
+        yield date1 + datetime.timedelta(n)
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Mocking Future Price data... saving to database')
-        for x in range(1,32):
-            if x < 10:
-                day = '0' + str(x)
-            else:
-                day = str(x)
-            d = '2018-01-' + day 
-            p = MockFuturePrice(date=d,price=100*x - random.randint(-20,20))
+
+        start_dt = datetime.datetime.now() - datetime.timedelta(days=97)
+        end_dt = datetime.datetime.now() + datetime.timedelta(days=7)
+        i = 7000
+        for dt in daterange(start_dt, end_dt):
+            p = MockFuturePrice(date=dt,price=i - random.randint(-30,30))
             p.save()
-            
+            i += 100 - random.randint(-30,30)
         self.stdout.write('Done!')
--- bitcoin_predict/btc/management/commands/mock_prices.py
+++ bitcoin_predict/btc/management/commands/mock_prices.py
@@ -5,17 +5,20 @@ import datetime
 from btc.models import MockPrice
 import requests
 from django.core.management.base import BaseCommand, CommandError
+import random 
+
+def daterange(date1, date2):
+    for n in range(int ((date2 - date1).days)+1):
+        yield date1 + datetime.timedelta(n)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Mocking Price data... saving to database')
-        for x in range(1,32):
-            if x < 10:
-                day = '0' + str(x)
-            else:
-                day = str(x)
-            d = '2018-01-' + day 
-            p = MockPrice(date=d,price=100*x)
+        start_dt = datetime.datetime.now() - datetime.timedelta(days=90)
+        end_dt = datetime.datetime.now()
+        i = 7000
+        for dt in daterange(start_dt, end_dt):
+            p = MockPrice(date=dt,price=i)
             p.save()
-            
+            i += 100 - random.randint(-20,20)
         self.stdout.write('Done!')
